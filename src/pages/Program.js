371f4ae394509668642d33ec6e859cfaec7997cc
gabi <ggkostova1106@gmail.com>
2023-04-24 11:45:48 +0300

    ProgramPage, ProfilePage, css

--- src/pages/Program.js
+++ src/pages/Program.js
@@ -5,85 +5,89 @@ import ProgramCard from "../components/ProgramCard";
 import MOVIES from "../movies";
 
 export default function MovieProgram() {
-  const location = useLocation();
-  const searchParams = new URLSearchParams(location.search);
-  const initialSelectedCinema = searchParams.get("cinema") || "";
-  console.log(initialSelectedCinema);
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+    const initialSelectedCinema = searchParams.get("cinema") || "";
+    const [selectedCinema, setSelectedCinema] = useState('');
+    const [movies, setMovies] = useState(MOVIES);
+    const [selectedMovie, setSelectedMovie] = useState('');
+    const [filteredMovies, setFilteredMovies] = useState([]);
 
-  const [selectedCinema, setSelectedCinema] = useState('');
+    useEffect(() => {
+        setSelectedCinema(initialSelectedCinema);
+    }, [initialSelectedCinema]);
 
+    useEffect(() => {
+        console.log(selectedCinema);
+        if (selectedCinema === "default" || selectedCinema === '') {
+            setMovies(MOVIES);
+            setFilteredMovies(MOVIES);
+        } else {
+            const filtered = MOVIES.filter((movie) => 
+                movie.cinemas.some((cinema) => cinema.name === selectedCinema)
+            )
+            setMovies(filtered);
+            setFilteredMovies(filtered);
+        }
+    }, [selectedCinema]);
 
-  const [movies, setMovies] = useState(MOVIES);
-  const [selectedMovie, setSelectedMovie] = useState("");
+    useEffect(() => {
+        if (selectedMovie === "default" || selectedMovie === '') {
+            setMovies(MOVIES);
+            setFilteredMovies(MOVIES);
+        } else if(selectedCinema !== "default" && !selectedCinema && (selectedMovie === "default" || selectedMovie === '')){
+            setMovies(filteredMovies);
+        } 
+        else {
+            const filtered = MOVIES.filter((movie) =>
+                movie.name.includes(selectedMovie)
+            );
+            setMovies(filtered);
+            setFilteredMovies(filtered);
+        }
+    }, [selectedMovie]);
 
-  useEffect(() => {
-    setSelectedCinema(initialSelectedCinema);
-  }, [initialSelectedCinema]);
-  
-  useEffect(() => {
-    console.log(selectedCinema);
-    if (selectedCinema === "default") {
-      setMovies(MOVIES);
-    } else {
-      const filteredMovies = MOVIES.filter((movie) =>
-        movie.cinemas.some((cinema) => cinema.name === selectedCinema)
-      );
-      setMovies(filteredMovies);
-    }
-  }, [selectedCinema]);
-
-  useEffect(() => {
-    if (selectedMovie === "default") {
-      setMovies(MOVIES);
-    } else {
-      const filteredMovies = MOVIES.filter((movie) =>
-        movie.name.includes(selectedMovie)
-      );
-      setMovies(filteredMovies);
-    }
-  }, [selectedMovie]);
-
-  return (
-    <div className="program-page">
-      <h2>Movie Program</h2>
-      <div className="program-filters">
-        <select
-          id="cinema"
-          name="cinema"
-          value={selectedCinema}
-          onChange={(e) => setSelectedCinema(e.target.value)}
-          required
-          style={{ height: "30px" }}
-        >
-          <option value="default">Choose cinema...</option>
-          <option value="КИНО АРЕНА GRAND MALL ВАРНА">
-            КИНО АРЕНА GRAND MALL ВАРНА
-          </option>
-          <option value="КИНО АРЕНА ДЕЛУКС BULGARIA MALL">
-            КИНО АРЕНА ДЕЛУКС BULGARIA MALL
-          </option>
-          <option value="КИНО АРЕНА МОЛ ВАРНА">КИНО АРЕНА МОЛ ВАРНА</option>
-          <option value="КИНО АРЕНА МОЛ МАРКОВО ТЕПЕ ПЛОВДИВ">
-            КИНО АРЕНА МОЛ МАРКОВО ТЕПЕ ПЛОВДИВ
-          </option>
-          <option value="КИНО АРЕНА THE MALL">КИНО АРЕНА THE MALL</option>
-        </select>
-        <select
-          value={selectedMovie}
-          onChange={(e) => setSelectedMovie(e.target.value)}
-        >
-          <option value="default">Select a movie...</option>
-          {MOVIES.map((movie) => (
-            <option key={movie.id} value={movie.name}>
-              {movie.name}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div className="movie-list-program">
-        {movies &&
-          movies.map((movie) => <ProgramCard key={movie.id} movie={movie} />)}
-      </div>
-    </div>
-  );
+    return (
+        <div className="program-page">
+            <h2>Movie Program</h2>
+            <div className="program-filters">
+                <select
+                    id="cinema"
+                    name="cinema"
+                    value={selectedCinema}
+                    onChange={(e) => setSelectedCinema(e.target.value)}
+                    required
+                    style={{ height: "30px" }}
+                >
+                    <option value="default">Choose cinema...</option>
+                    <option value="КИНО АРЕНА GRAND MALL ВАРНА">
+                        КИНО АРЕНА GRAND MALL ВАРНА
+                    </option>
+                    <option value="КИНО АРЕНА ДЕЛУКС BULGARIA MALL">
+                        КИНО АРЕНА ДЕЛУКС BULGARIA MALL
+                    </option>
+                    <option value="КИНО АРЕНА МОЛ ВАРНА">КИНО АРЕНА МОЛ ВАРНА</option>
+                    <option value="КИНО АРЕНА МОЛ МАРКОВО ТЕПЕ ПЛОВДИВ">
+                        КИНО АРЕНА МОЛ МАРКОВО ТЕПЕ ПЛОВДИВ
+                    </option>
+                    <option value="КИНО АРЕНА THE MALL">КИНО АРЕНА THE MALL</option>
+                </select>
+                <select
+                    value={selectedMovie}
+                    onChange={(e) => setSelectedMovie(e.target.value)}
+                >
+                    <option value="default">Select a movie...</option>
+                    {filteredMovies.map((movie) => (
+                        <option key={movie.id} value={movie.name}>
+                            {movie.name}
+                        </option>
+                    ))}
+                </select>
+            </div>
+            <div className="movie-list-program">
+                {movies &&
+                    movies.map((movie) => <ProgramCard key={movie.id} movie={movie} />)}
+            </div>
+        </div>
+    );
 }
